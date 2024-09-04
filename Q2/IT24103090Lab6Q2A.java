72b6ee75bc8bdceef2d724a95b45c11501816829
IT24103090 <it24103090@my.sliit.lk>
2024-09-04 03:31:10 -0700

    IT24103090Lab6Q2A

---
+++ Q2/IT24103090Lab6Q2A.java
@@ -0,0 +1,13 @@
+public class IT24103090Lab6Q2A {
+    public static void main(String[] args) {
+        int counter = 1;  
+
+        while (counter <= 10) {
+            
+            System.out.print(counter + " ");
+            
+            
+            counter++;
+        }
+    }
+}
